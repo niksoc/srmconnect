6a0df106fca7314b1dd2bc82f5d19f70bf884e86
niksoc <nikhil_2811@hotmail.com>
2016-09-17 22:22:15 +0530

    fixed bug in timestamp

--- app/js/components/Timestamp.js
+++ app/js/components/Timestamp.js
@@ -6,7 +6,7 @@ const Timestamp = (props) => {
     const datetime = props.datetime.replace('T',' ');
     const date = datetime.split(' ')[0].split('-');
     const day = date[2];
-    const month = monthStrings[Number(date[1])];
+    const month = monthStrings[Number(date[1])-1];
     const year = "'" + date[0].slice(2); 
     const time = datetime.split(' ')[1].slice(0,-3);
     const datetime_str = [day, month, year, 'at', time].join(' ');
